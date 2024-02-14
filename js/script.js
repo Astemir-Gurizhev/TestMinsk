d45a4efcb480328ac2925f254caa785bf1110580
Astemir Gurizhev <gurizhev.astema@mail.ru>
2024-02-14 17:31:45 +0300

    testhh08

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ const valueRange = document.querySelector('.ranges__text-right')
 range.oninput = () => {
     valueRange.textContent = range.value + '%'
 }
+
+const select = document.querySelector('.form__top-select')
+const selectList = document.querySelector('.top__select-list')
+const selectWrapper = document.querySelector('.top__select-wrapper')
+const selectImg = document.querySelector('.top__select-img')
+select.addEventListener('click', e => {
+    selectList.classList.toggle('active')
+    selectWrapper.classList.toggle('active')
+    selectImg.classList.toggle('active')
+    select.classList.toggle('active')
+})
