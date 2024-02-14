10876a3dda773c48d11c8000eafc20f94786bb81
Astemir Gurizhev <gurizhev.astema@mail.ru>
2024-02-14 18:14:57 +0300

    testhh09

--- js/script.js
+++ js/script.js
@@ -14,3 +14,27 @@ select.addEventListener('click', e => {
     selectImg.classList.toggle('active')
     select.classList.toggle('active')
 })
+
+
+const textType = document.querySelector('.top__select-type')
+const selectListItem = document.querySelectorAll('.select__list-item')
+
+selectListItem.forEach(item => {
+    item.addEventListener('click', () => {
+        textType.innerHTML = item.textContent
+    })
+})
+
+// const formBtn = document.querySelector('.second__form-btn')
+// formBtn.addEventListener('click', e => {
+//     e.preventDefault()
+// })
+
+const inputFile = document.querySelector('.form__bottom-inputFile')
+const fileText = document.querySelector('.bottom__file-text')
+
+inputFile.addEventListener('input', () => {
+    inputFile.value != '' ? fileText.innerHTML = inputFile.value : fileText.innerHTML = 'Прикрепить файл'
+})
+
+
